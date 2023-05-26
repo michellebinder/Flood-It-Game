889ecbb6ec88166fe77d130d63651c71eb6cf62a
root <root@progserver.informatik.uni-koeln.de>
2023-05-26 11:52:36 +0200

    init

---
+++ src/start/Start.java
@@ -0,0 +1,15 @@
+package start;
+
+/*
+ * Siehe Hinweise auf dem Aufgabenblatt.  
+ */
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ src/testing/Testing.java
@@ -0,0 +1,75 @@
+package testing;
+
+/*
+ * Siehe Hinweise auf dem Aufgabenblatt. 
+ */
+
+import logic.Field;
+
+public class Testing {
+
+	private Field[][] board;
+
+	public Testing(Field[][] initBoard) {
+		this.board = initBoard;
+		
+		
+	}
+
+	public boolean isStartklar() {
+
+		return false;
+	}
+
+	public boolean isEndConfig() {
+
+		return false;
+	}
+
+	
+	public int testStrategy01() {
+
+		return 0;
+	}
+
+	public int testStrategy02() {
+
+		return 0;
+	}
+
+	public int testStrategy03() {
+
+		return 0;
+	}
+
+	
+	public boolean toBoard(Field[][] anotherBoard, int moves) {
+		
+		return false;
+	}
+
+	public int minMoves(int row, int col) { 
+
+		return 0;
+	}
+
+	public int minMovesFull() {
+
+		return 0;
+	}
+
+	
+	
+	
+	/*
+	 * Getter und Setter
+	 */
+	public Field[][] getBoard() {
+		return board;
+	}
+
+	public void setBoard(Field[][] board) {
+		this.board = board;
+	}
+
+}
